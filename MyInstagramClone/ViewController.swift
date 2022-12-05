c29e60ca7ca2d431a21dc2be34b9cb2c656bd9de
ymyldz <113609896+ymyldz@users.noreply.github.com>
2022-12-05 16:40:26 +0300

    Initial Commit

---
+++ MyInstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyInstagramClone
+//
+//  Created by Yusuf Mert Yıldız on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
